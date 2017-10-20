e596c7017ef830c4680069f62566a8d205ae3942
Rasmus Storjohann <rasmus@peacegeeks.org>
2017-10-20 11:28:42 -0700

    Issue #100 Refactor urls file

--- polls/api.py
+++ polls/api.py
@@ -1,21 +1,11 @@
 from rest_framework import routers
 from polls import views
 
-def build_url_patterns():
+def url_patterns_for_question_and_choice():
     router = routers.DefaultRouter()
-    register_question(router)
-    register_choice(router)
+    router.register(r'^questions', views.QuestionViewSet)
+    router.register(r'^questions/(?P<question_id>[0-9]+)/choices', views.ChoiceViewSet, 'choice')
     return router.urls
 
-def register_question(router):
-    pattern = r'^questions'
-    question_view_set = views.QuestionViewSet
-    router.register(pattern, question_view_set)
-
-def register_choice(router):
-    pattern_capturing_id = r'^questions/(?P<question_id>[0-9]+)/choices'
-    choice_view_set = views.ChoiceViewSet
-    router.register(pattern_capturing_id, choice_view_set, base_name='choice')
-
 # pylint: disable=invalid-name
-urlpatterns = build_url_patterns()
+urlpatterns = url_patterns_for_question_and_choice()
