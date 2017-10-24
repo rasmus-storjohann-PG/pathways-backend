8b4e95116800756957af60a7d66f712287d37f2c
Rasmus Storjohann <rasmus@peacegeeks.org>
2017-10-24 08:58:42 -0700

    Issue #100 Remove comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,37 +1,5 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
-
-# v1/polls/
-#   GET
-#   POST
-# v1/polls/<id>/
-#   GET
-#   PUT
-#   DELETE
-# v1/polls/<id>/choices/
-#   GET
-#   POST
-# v1/polls/<id>/choices/<id>/
-#   GET
-#   PUT
-#   DELETE
-# v1/polls/<id>/choices/<id>/vote/
-#   POST
 
 # pylint: disable=invalid-name
 urlpatterns = [
