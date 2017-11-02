86c855684e7da4eb8dc46bc0a75b279ec82ffbf9
rasmus <rasmus@peacegeeks.org>
2017-11-02 12:15:26 -0700

    Issue #118 Add latitude and longitude to model class

--- service_providers/models.py
+++ service_providers/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class ServiceProvider(models.Model):
     name = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
--- service_providers/tests/storage/test_service_provider_model.py
+++ service_providers/tests/storage/test_service_provider_model.py
@@ -1,10 +1,21 @@
 from django.test import TestCase
 from service_providers import models
+from decimal import *
 
 class TestServiceProviderModel(TestCase):
     def test_can_create_service_provider(self):
         provider = models.ServiceProvider()
         provider.name = "The name"
+        provider.latitude = 123.456
+        provider.longitude = 123.456
         provider.save()
         provider_from_db = models.ServiceProvider.objects.get()
         self.assertEqual(provider_from_db.name, "The name")
+
+    def test_has_latitude(self):
+        provider = models.ServiceProvider()
+        provider.latitude = 123.456
+        provider.longitude = 123.456
+        provider.save()
+        provider_from_db = models.ServiceProvider.objects.get()
+        self.assertEqual(provider_from_db.latitude, Decimal('123.456000'))
