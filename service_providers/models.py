7aefa511fab6e58c8d375eae7886f5f341a92f1c
rasmus <rasmus@peacegeeks.org>
2017-11-03 14:00:44 -0700

    Issue #118 Add admin support for the service provider model

--- service_providers/models.py
+++ service_providers/models.py
@@ -8,3 +8,6 @@ class ServiceProvider(TranslatableModel):
     translations = TranslatedFields(
         description=models.TextField(null=True)
     )
+
+    def __str__(self):
+        return self.name
