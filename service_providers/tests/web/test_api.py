5c72df4c5ddd44b9ccdaf8c6527583e04d28fc77
rasmus <rasmus@peacegeeks.org>
2017-11-04 21:26:48 -0700

    Issue #119 Make service provider REST endpoint read-only

--- service_providers/tests/web/test_api.py
+++ service_providers/tests/web/test_api.py
@@ -26,3 +26,22 @@ class ServiceProvidersApiTests(rest_test.APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['description'], 'Service description')
+
+    def test_cannot_post(self):
+        url = '/v1/service_providers/'
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_cannot_put(self):
+        provider = ServiceProviderBuilder().build()
+        provider.save()
+        url = '/v1/service_providers/{0}/'.format(provider.pk)
+        response = self.client.put(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_cannot_delete(self):
+        provider = ServiceProviderBuilder().build()
+        provider.save()
+        url = '/v1/service_providers/{0}/'.format(provider.pk)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
