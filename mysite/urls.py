e5c82c96df7b7c71d87d5f2e7b2fc6aba02381a9
rasmus <rasmus@peacegeeks.org>
2017-11-04 21:31:22 -0700

    Issue #119 Fix URI to contain hyphen rather than underscore

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^v1/forms/polls/', include('polls.web.urls')),
     url(r'^v1/polls/', include('polls.web.api')),
-    url(r'^v1/service_providers', include('service_providers.web.urls')),
+    url(r'^v1/service-providers', include('service_providers.web.urls')),
     url(r'^v1/admin/', admin.site.urls),
 ]
--- service_providers/tests/web/test_api.py
+++ service_providers/tests/web/test_api.py
@@ -14,7 +14,7 @@ class ServiceProvidersApiTests(rest_test.APITestCase):
     def test_can_get_entities(self):
         ServiceProviderBuilder().with_name('First').build().save()
         ServiceProviderBuilder().with_name('Second').build().save()
-        url = '/v1/service_providers/'
+        url = '/v1/service-providers/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()), 2)
@@ -22,26 +22,26 @@ class ServiceProvidersApiTests(rest_test.APITestCase):
     def test_can_get_one_entity(self):
         provider = ServiceProviderBuilder().with_description('Service description').build()
         provider.save()
-        url = '/v1/service_providers/{0}/'.format(provider.pk)
+        url = '/v1/service-providers/{0}/'.format(provider.pk)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['description'], 'Service description')
 
     def test_cannot_post(self):
-        url = '/v1/service_providers/'
+        url = '/v1/service-providers/'
         response = self.client.post(url, self.data)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cannot_put(self):
         provider = ServiceProviderBuilder().build()
         provider.save()
-        url = '/v1/service_providers/{0}/'.format(provider.pk)
+        url = '/v1/service-providers/{0}/'.format(provider.pk)
         response = self.client.put(url, self.data)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cannot_delete(self):
         provider = ServiceProviderBuilder().build()
         provider.save()
-        url = '/v1/service_providers/{0}/'.format(provider.pk)
+        url = '/v1/service-providers/{0}/'.format(provider.pk)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
