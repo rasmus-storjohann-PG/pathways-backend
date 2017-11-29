9b5b53368b1644643ee36a5d9b9853fdf4d651d1
rasmus <rasmus@peacegeeks.org>
2017-11-29 09:23:47 -0800

    Issue #129 Tweak the model cleanup code to replace '' with null regardless of whether null            is a valid value for the column, that way we get database integrity errors when            trying to save '' to a column where empty values are not valid, which is a very            good thing.

--- common/models.py
+++ common/models.py
@@ -22,14 +22,11 @@ class ValidateOnSaveMixin(object):
 
     def set_empty_fields_to_null(self):
         for field in self.all_fields():
-            if self.can_be_null(field) and self.is_empty(field):
+            if self.is_empty(field):
                 self.set_to_null(field)
 
     def all_fields(self):
         return self._meta.fields
-
-    def can_be_null(self, field):
-        return field.null
 
     def is_empty(self, field):
         value = getattr(self, field.attname)
