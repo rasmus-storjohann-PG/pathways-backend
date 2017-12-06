c8a3b47ce7514220d7371c31834af57f5fd81dd8
Unknown <rasmus@peacegeeks.org>
2017-12-06 11:09:59 -0800

    Issue #129 Explicitly handle required and optional fields in the parser

--- bc211/parser.py
+++ bc211/parser.py
@@ -19,7 +19,7 @@ def parse_agency(agency):
     id = parse_agency_key(agency)
     name = parse_agency_name(agency)
     description = parse_agency_description(agency)
-    website = parse_agency_website(agency)
+    website = parse_agency_website_with_prefix(agency)
     email = parse_agency_email(agency)
     LOGGER.info('Parsed organization: %s %s', id, name)
     locations = parse_sites(agency, id)
@@ -27,28 +27,35 @@ def parse_agency(agency):
                              email=email, locations=locations)
 
 def parse_agency_key(agency):
-    return agency.find('Key').text
+    return parse_required_field(agency, 'Key')
+
+def parse_required_field(parent, field):
+    try:
+        return parent.find(field).text
+    except AttributeError:
+        raise MissingRequiredFieldXmlParseException(field)
+
+def parse_optional_field(parent, field):
+    value = parent.find(field)
+    return None if value is None else value.text
 
 def parse_agency_name(agency):
-    return agency.find('Name').text
+    return parse_required_field(agency, 'Name')
 
 def parse_agency_description(agency):
-    return agency.find('AgencyDescription').text
+    return parse_required_field(agency, 'AgencyDescription')
 
 def parse_agency_email(agency):
-    email = agency.find('Email/Address')
-    return None if email is None else email.text
+    return parse_optional_field(agency, 'Email/Address')
 
-def parse_agency_website(agency):
-    website = agency.find('URL/Address')
-    if website is None:
-        return None
-    return website_with_http_prefix(website.text)
+def parse_agency_website_with_prefix(agency):
+    website = parse_optional_field(agency, 'URL/Address')
+    return None if website is None else website_with_http_prefix(website)
 
 def website_with_http_prefix(website):
     parts = urlparse.urlparse(website, 'http')
-    url_with_extra_slash = urlparse.urlunparse(parts)
-    return url_with_extra_slash.replace('///', '//')
+    whole_with_extra_slash = urlparse.urlunparse(parts)
+    return whole_with_extra_slash.replace('///', '//')
 
 def parse_sites(agency, organization_id):
     sites = agency.findall('Site')
@@ -71,17 +78,17 @@ def parse_site(site, organization_id):
                          description=description, spatial_location=spatial_location)
 
 def parse_site_id(site):
-    return site.find('Key').text
+    return parse_required_field(site, 'Key')
 
 def parse_site_name(site):
-    return site.find('Name').text
+    return parse_required_field(site, 'Name')
 
 def parse_site_description(site):
-    return site.find('SiteDescription').text
+    return parse_required_field(site, 'SiteDescription')
 
 def parse_spatial_location_if_defined(site):
-    latitude = site.find('SpatialLocation/Latitude')
-    longitude = site.find('SpatialLocation/Longitude')
+    latitude = parse_optional_field(site, 'SpatialLocation/Latitude')
+    longitude = parse_optional_field(site, 'SpatialLocation/Longitude')
     if latitude is None or longitude is None:
         return None
-    return dtos.SpatialLocation(latitude=latitude.text, longitude=longitude.text)
+    return dtos.SpatialLocation(latitude=latitude, longitude=longitude)
