05de966344c66e545e223a62f63839e29282c3a4
rasmus <rasmus@peacegeeks.org>
2017-12-15 14:30:11 -0800

    Issue #113 Remove redundant script, use ./manage.py generate_secret_key instead

--- utility/generate_django_secret.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import random
-
-secret = ''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50))
-print("'" + secret + "'")
